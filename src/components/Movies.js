c2730d93b29da8764394dc45a6a0c1c0750916a6
RobertTTaylor9517 <robt15@hotmail.com>
2020-02-04 10:16:29 -0500

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import { movies } from '../data';
 
+const renderMovies=()=>{
+  return movies.map(movie => {
+    return (
+      <div>
+        <h4>{movie.title}</h4>
+        <p>Time: {movie.time} min</p>
+        <ul>
+          {movie.genres.map(genre => <li>{genre}</li>)}
+        </ul>
+      </div>
+    )
+  })
+}
+
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {renderMovies()}
     </div>
   );
 };
